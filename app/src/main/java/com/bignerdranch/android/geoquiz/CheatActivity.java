4284d5e5933880dac1503364ccf49ab86d5fc2ab
Jeff Corpac <jcorpac@gmail.com>
2016-01-21 16:48:19 -0600

    End of Chapter 5

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -0,0 +1,53 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
+
+    private boolean mAnswerIsTrue;
+    private boolean mAnswerIsShown;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                mAnswerIsShown = true;
+                setAnswerShownResult(mAnswerIsShown);
+            }
+        });
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+}
